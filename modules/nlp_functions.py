429bdec912ffa0fe035ec2c7c96b13bdf06071af
RackSpaces <113273912+RackSpaces@users.noreply.github.com>
2024-05-17 21:36:33 +0000

    Update hello_world.md

--- modules/nlp_functions.py
+++ modules/nlp_functions.py
@@ -10,4 +10,51 @@ import pandas as pd
 from pylab import plt
 from wordcloud import WordCloud
 from nltk.corpus import stopwords
-from nltk.corpus import
+from nltk.corpus import wordnet as wn
+from lxml.html.clean import Cleaner
+from sklearn.feature_extraction.text import TfidfVectorizer
+plt.style.use('seaborn')
+
+cleaner = Cleaner(style=True, links=True, allow_tags=[''],
+                  remove_unknown_tags=False)
+
+stop_words = stopwords.words('english')
+stop_words.extend(['new', 'old', 'pro', 'open', 'menu', 'close'])
+
+
+def remove_non_ascii(s):
+    ''' Removes all non-ascii characters.
+    '''
+    return ''.join(i for i in s if ord(i) < 128)
+
+def clean_up_html(t):
+    t = cleaner.clean_html(t)
+    t = re.sub('[\n\t\r]', ' ', t)
+    t = re.sub(' +', ' ', t)
+    t = re.sub('<.*?>', '', t)
+    t = remove_non_ascii(t)
+    return t
+
+def clean_up_text(t, numbers=False, punctuation=False):
+    ''' Cleans up a text, e.g. HTML document,
+        from HTML tags and also cleans up the 
+        text body.
+    '''
+    try:
+        t = clean_up_html(t)
+    except:
+        pass
+    t = t.lower()
+    t = re.sub(r"what's", "what is ", t)
+    t = t.replace('(ap)', '')
+    t = re.sub(r"\'ve", " have ", t)
+    t = re.sub(r"can't", "cannot ", t)
+    t = re.sub(r"n't", " not ", t)
+    t = re.sub(r"i'm", "i am ", t)
+    t = re.sub(r"\'s", "", t)
+    t = re.sub(r"\'re", " are ", t)
+    t = re.sub(r"\'d", " would ", t)
+    t = re.sub(r"\'ll", " will ", t)
+    t = re.sub(r'\s+', ' ', t)
+    t = re.sub(r"\\", "", t)
+    t = re.sub(r"\'", "", t) 
